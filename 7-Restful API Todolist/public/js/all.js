ef559789ff29be1ca17e7440efd0428bed679cc6
Mars <talent.seek@msa.hinet.net>
2018-03-02 18:12:11 +0800

    20180302

---
+++ 7-Restful API Todolist/public/js/all.js
@@ -0,0 +1,26 @@
+var send = document.getElementById('send');
+var content = document.getElementById('content');
+var list = document.getElementById('list');
+
+send.addEventListener('click', function (e) {
+  var str = content.value;
+  var xhr = new XMLHttpRequest();
+  xhr.open('post', '/addTodo');
+  xhr.setRequestHeader('Content-type', "application/json");
+  var todo = JSON.stringify({ "content": str })
+  xhr.send(todo);
+  xhr.onload = function () {
+    var originData = JSON.parse(xhr.responseText);
+    console.log(originData);
+    if (originData.success == false) {
+      alert(originData.message);
+      return;
+    }
+    var data = originData.result;
+    var str = '';
+    for (item in data) {
+      str += '<li>' + data[item].content + '</li>';
+    }
+    list.innerHTML = str;
+  }
+})
