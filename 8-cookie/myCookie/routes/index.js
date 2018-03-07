b0f95a1a4692ae9f481c4c8b5226271d637db957
Mars <talent.seek@msa.hinet.net>
2018-03-07 11:32:12 +0800

    20180307

--- 8-cookie/myCookie/routes/index.js
+++ 8-cookie/myCookie/routes/index.js
@@ -2,12 +2,30 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  console.log(req.cookies);
-  res.cookie('name','Mary',{
-    maxAge: 10000
+router.get('/', function (req, res, next) {
+  // console.log(req.cookies);
+  // console.log(req.session);
+  // req.session.username = 'tom';
+  // req.session.email = 'aaa@com.tw';
+  // res.cookie('name','Mary',{
+  //   maxAge: 10000
+  // })
+
+  //res.render('index', { title: 'Express' });
+  res.render('index',{
+    userName: req.session.username,
+    email: req.session.email
   })
-  res.render('index', { title: 'Express' });
 });
+
+
+router.post('/', function(req, res){
+  req.session.username = req.body.username;
+  req.session.email = req.body.email;
+  res.redirect('/');
+})
+
+
+
 
 module.exports = router;
